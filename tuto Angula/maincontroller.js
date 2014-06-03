f649b8cb299829eda872148bef6c4f437130eb6d
Alaric Calmette <alaric.calmette@gmail.com>
2014-06-03 15:55:29 +0200

    add primitives and childhood

--- tuto Angula/maincontroller.js
+++ tuto Angula/maincontroller.js
@@ -2,7 +2,28 @@ app.controller("MainController", function($scope,$http){
 
  $scope.blocks = [];
   $scope.addBlock = function(){
+    if($scope.parentblock == null)
+    {
+      var div = document.createElement("div");
+      div.setAttribute('id',$scope.blocktitle);
+      div.setAttribute('lvl',0);
+      div.innerHTML = $scope.blocktitle;
+      document.getElementById('blocks').appendChild(div);
       $scope.blocks.push([$scope.blocktitle,$scope.selectedtype.tagname,$scope.parentblock]);
+
+    }
+    else
+    {
+      var div = document.createElement("div");
+      div.setAttribute('id',$scope.blocktitle);
+      var lvl = Number(document.getElementById($scope.parentblock[0]).getAttribute('lvl'))+1;
+      div.setAttribute('lvl',lvl);
+      div.innerHTML = (new Array(lvl + 1).join("&nbsp")) + $scope.blocktitle;
+      document.getElementById($scope.parentblock[0]).appendChild(div);
+      $scope.blocks.push([$scope.blocktitle,$scope.selectedtype.tagname,$scope.parentblock]);
+
+    }
+      
     }
 
   $scope.test = "";
@@ -19,12 +40,12 @@ app.controller("MainController", function($scope,$http){
 
   $scope.types=null;
   $http.get('types.json')
-  	.success(function(data) {
-  		$scope.types=data;
-  	})
-  	.error(function(data,status,error,config){
+    .success(function(data) {
+      $scope.types=data;
+    })
+    .error(function(data,status,error,config){
           $scope.types = [{heading:"Error",description:status}];
       });
-  	
+    
     
 });
