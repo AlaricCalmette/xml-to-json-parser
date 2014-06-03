2150ecc3e640cc6dfa3d459f704e23f9b87a83f0
Alaric Calmette <alaric.calmette@gmail.com>
2014-06-03 17:33:25 +0200

    add conditional input

--- tuto Angula/maincontroller.js
+++ tuto Angula/maincontroller.js
@@ -7,9 +7,17 @@ app.controller("MainController", function($scope,$http){
       var div = document.createElement("div");
       div.setAttribute('id',$scope.blocktitle);
       div.setAttribute('lvl',0);
-      div.innerHTML = $scope.blocktitle;
+      var path = '/'+$scope.selectedtype.tagname;
+      div.setAttribute('path',path);
+
+      div.innerHTML = '<h1>' + $scope.blocktitle + '</h1><p> path : '+ path + '</p>';
+      if($scope.blockvalue)
+      {
+        div.innerHTML = div.innerHTML + '<p>' + $scope.blockvalue + '</p>';
+      }
+
       document.getElementById('blocks').appendChild(div);
-      $scope.blocks.push([$scope.blocktitle,$scope.selectedtype.tagname,$scope.parentblock]);
+      $scope.blocks.push([$scope.blocktitle,$scope.selectedtype.tagname,$scope.parentblock,path]);
 
     }
     else
@@ -18,15 +26,24 @@ app.controller("MainController", function($scope,$http){
       div.setAttribute('id',$scope.blocktitle);
       var lvl = Number(document.getElementById($scope.parentblock[0]).getAttribute('lvl'))+1;
       div.setAttribute('lvl',lvl);
-      div.innerHTML = (new Array(lvl + 1).join("&nbsp")) + $scope.blocktitle;
+      var path = ($scope.parentblock[3]+'/'+$scope.selectedtype.tagname);
+
+      div.innerHTML = '<h2>' + (new Array(lvl + 1).join("&nbsp")) + $scope.blocktitle +
+       '</h2><p>' +  (new Array(lvl + 5).join("&nbsp")) + ' path : '+ path + '</p>';
+
+       if($scope.blockvalue)
+      {
+        div.innerHTML = div.innerHTML +  '<p>' + (new Array(lvl + 5).join("&nbsp")) + 
+        $scope.blockvalue + '</p>';
+      }
+
       document.getElementById($scope.parentblock[0]).appendChild(div);
-      $scope.blocks.push([$scope.blocktitle,$scope.selectedtype.tagname,$scope.parentblock]);
+      $scope.blocks.push([$scope.blocktitle,$scope.selectedtype.tagname,$scope.parentblock,path]);
 
     }
       
     }
 
-  $scope.test = "";
   $scope.contents = null;
  
   $scope.selectedtype = null;
@@ -46,6 +63,8 @@ app.controller("MainController", function($scope,$http){
     .error(function(data,status,error,config){
           $scope.types = [{heading:"Error",description:status}];
       });
-    
+
+    $scope.primitives = ["gco:CharacterString","Real","Integer","Decimal","Url","gco:Date","gco:DateTime","gco:Measure"];
+
     
 });
